6afe86fa9465e474391b274e9190877460b35d61
harshmayan <harshmayani07@gmail.com>
2023-09-30 10:49:06 +0530

    node modules setup and organization of app folders

---
+++ lib/utils.ts
@@ -0,0 +1,7 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+// cn is utils helps to create dynamic classes inside tailwind ,clsx and tailwindMerge...
