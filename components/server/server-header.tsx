e267d87146ef24de92014b2d0ccb7b9f1e1f87c7
harshmayan <harshmayani07@gmail.com>
2023-10-17 11:57:38 +0530

    All of the second sidebar lists-- frontend and functionality completed

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -78,6 +78,7 @@ export const ServerHeader =({
                 )}
                 {isModerator && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("createChannel")}
                         className="px-3 py-2 text-sm cursor-pointer"
                     >
                         Create Channel
@@ -89,6 +90,7 @@ export const ServerHeader =({
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("deleteServer",{ server })}
                         className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
                     >
                     Delete Server
@@ -97,6 +99,7 @@ export const ServerHeader =({
                 )}
                 {!isAdmin && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("leaveServer", { server })}
                         className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
                     >
                     Leave Server
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,4 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-// cn is utils helps to create dynamic classes inside tailwind ,clsx and tailwindMerge...
